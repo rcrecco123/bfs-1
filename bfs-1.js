3880128b26c05cc6ad9288aeff8668937c0d0b48
Ronald Crecco <ronaldcrecco@Ronalds-MacBook-Pro.local>
2020-10-01 15:53:23 -0400

    first commit

---
+++ bfs-1.js
@@ -0,0 +1,21 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+function breadthFirst() {
+  let viewed = [];
+  let queue = [];
+  let current = this.root;
+  queue.push(current);
+  while (queue.length) {
+    current = queue.shift();
+    viewed.push(current.value);
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return viewed;
+  //should return all node values in breadth first order
+}
